933e3613bb2ee0e84acb99c61c25f53c9857125d
Ваше Имя <you@example.com>
2020-03-24 11:17:07 +0300

    1

--- cpp/3.cpp
+++ cpp/3.cpp
@@ -8,7 +8,7 @@ const double b =2;
 
 double F(double x,double y)
 {
-	double result = a*x+b*y;
+	double result = log(abs((x-a)/(y-b)))+exp(x/a)* (pow(x,3)+a*pow(x,2)+pow(a,2)*x+pow(a,3)) / pow(y-b,2);
 	return result ;
 }
 
